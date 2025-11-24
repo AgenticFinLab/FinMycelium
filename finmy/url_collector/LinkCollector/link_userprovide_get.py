2a4b247c9b62df5e0fefa740555397c57d7725c8
YuqunZhang <zhangyuqun0909@gmail.com>
2025-11-24 18:02:32 +0800

    feat: add the structure of link_userprovide_get.py

---
+++ finmy/url_collector/LinkCollector/link_userprovide_get.py
@@ -0,0 +1,20 @@
+"""
+Process URL links or files provided by user
+
+"""
+
+
+def url_csv_file():
+    pass
+
+
+def url_json_file():
+    pass
+
+
+def url_single():
+    pass
+
+
+if __name__ == "__main__":
+    pass
