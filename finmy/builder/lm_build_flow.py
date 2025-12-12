6197dfceb2809994c7bed05dbfa3e06a5e32dbf2
YuqunZhang <zhangyuqun0909@gmail.com>
2025-12-12 19:36:54 +0800

    fix: remove unnecessary part

--- finmy/builder/lm_build_flow.py
+++ finmy/builder/lm_build_flow.py
@@ -388,7 +388,3 @@ def lm_build_flow_main(raw_texts: List[str], query_text: str, key_words: List[st
     logger.info("Test flow completed successfully!")
 
 
-if __name__ == "__main__":
-    raw_texts = get_sample_raw_texts()
-    timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
-    lm_build_flow_main(raw_texts=raw_texts, query_text="识别与人工智能在金融风控与合规相关的内容", key_words=["人工智能", "AI", "风险管理", "模型合规", "透明度"],output_dir=f"./examples/utest/Collector/test_files/event_output_{timestamp}")
